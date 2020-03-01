496450b7d8e619c66ee0f93785cc225a5e511b29
unknown <mrkarki2@gmail.com>
2020-03-01 12:50:44 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInpute';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoInput/>
+      <TodoList/>
+<h1>Hello from app</h1>
     </div>
   );
 }
---
+++ src/components/TodoInpute.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInpute extends Component {
+    render() {
+        return (
+            <div>
+                hello from input
+            </div>
+        )
+    }
+}
