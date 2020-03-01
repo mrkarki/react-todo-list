4e5b37f7aa96a120ec104f88785c2c08c48655a4
unknown <mrkarki2@gmail.com>
2020-03-01 14:27:52 +0545

    add methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 //import './App.css';
 import uuid from 'uuid';
@@ -6,14 +6,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInpute';
 import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <TodoInput/>
-      <TodoList/>
-<h1>Hello from app</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    items: [{ id: 1, title: "wakeup" }, { id: 1, title: "wakeup" }],
+    //id: uuid(),
+    item: '',
+    editItem: false
+  };
+  handelChange = (e) => { console.log('handel change') }
+  handelSubmit = (e) => { console.log('handel submit') }
+  clearList = () => { console.log('clear list') }
+  handelDelete = (id) => { console.log(`handel delete ${id}`) }
+  handelEdit = (id) => { console.log(`handel edit ${id}`) }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitilize text-center">
+              todo input
+            <TodoInput item={this.state.item} handelChange={this.handelChange} handelSubmit={this.handelSubmit} editItem={this.state.editItem}/>
+              <TodoList items={this.state.items} clearList={this.clearList} handelDelete={this.handelDelete} handelEdit={this.handelEdit}/>
+            </h3>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
